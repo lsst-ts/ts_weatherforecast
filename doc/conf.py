b2c31ec198470f8e74b5873d94b2f0f8d987bfda
Eric Coughlin <ecoughlin@lsst.org>
2023-03-22 11:39:56 -0300

    Use generate_pre_commit_conf

--- doc/conf.py
+++ doc/conf.py
@@ -4,8 +4,8 @@ from documenteer.conf.pipelinespkg import *  # type: ignore # noqa
 from lsst.ts import weatherforecast  # type: ignore # noqa
 
 project = "ts_weatherforecast"
-html_theme_options["logotext"] = project
-html_title = project
-html_short_title = project
+html_theme_options["logotext"] = project  # noqa
+html_title = project  # noqa
+html_short_title = project  # noqa
 
-intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)
+intersphinx_mapping["ts_xml"] = ("https://ts-xml.lsst.io", None)  # noqa
